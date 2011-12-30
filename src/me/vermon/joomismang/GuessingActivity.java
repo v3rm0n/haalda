5eb614109438cf96f483ebe74df3edf92523e2f9
Maido Käära <vermon@gmail.com>
2011-12-30 03:29:34 +0200

    First working version

--- src/me/vermon/joomismang/GuessingActivity.java
+++ src/me/vermon/joomismang/GuessingActivity.java
@@ -1,8 +1,11 @@
 package me.vermon.joomismang;
 
 import java.util.List;
+import java.util.Random;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.speech.RecognitionListener;
@@ -11,33 +14,77 @@ import android.speech.SpeechRecognizer;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class GuessingActivity extends Activity{
-    
-    private static final String TAG = GuessingActivity.class.getName();
-    private SpeechRecognizer spc;
 
-    
+    private static final String TAG = GuessingActivity.class.getName();
+    public static final String EXTRA_LEVEL = "EXTRA_LEVEL";
+    private SpeechRecognizer spc;
+    private String currentWord;
+    private int level = R.array.first_level;
+
     @Override
     public void onCreate( Bundle savedInstanceState ){
         super.onCreate( savedInstanceState );
+        Intent intent = getIntent();
+        level = intent.getIntExtra( EXTRA_LEVEL, R.array.first_level );
         setContentView( R.layout.guess );
+        changeWord();
         this.spc = SpeechRecognizer.createSpeechRecognizer( this );
         spc.setRecognitionListener( rl );
         final Button speak = (Button)findViewById( R.id.b_speak );
         speak.setOnClickListener( new View.OnClickListener(){
 
             public void onClick( View v ){
-                guess();
+                listen();
+                speak.setVisibility( View.GONE );
             }
         } );
     }
-    
-    public void guess(){
-        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intent.putExtra( RecognizerIntent.EXTRA_PROMPT, "Räägi raisk" );
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        spc.destroy();
+    }
+
+    private void listen(){
+        Intent intent = new Intent( RecognizerIntent.ACTION_RECOGNIZE_SPEECH );
         spc.startListening( intent );
+    }
+
+    private void changeWord(){
+        currentWord = getRandomWord();
+        final TextView word = (TextView)findViewById( R.id.word );
+        word.setText( "\"" + currentWord + "\"" );
+    }
+
+    private String getRandomWord(){
+        CharSequence[] firstLevel = getResources().getTextArray( level );
+        Random r = new Random();
+        int index = r.nextInt( firstLevel.length );
+        return firstLevel[index].toString();
+    }
+
+    private void showDialog( boolean success, String match ){
+        AlertDialog.Builder builder = new AlertDialog.Builder( this );
+        String message = success ? "Väga tubli. Proovid edasi?" : "Sina ütlesid " + match + ". Proovid uuesti?";
+        builder.setMessage( message ).setCancelable( false ).setPositiveButton( "Jah", new DialogInterface.OnClickListener(){
+
+            public void onClick( DialogInterface dialog, int id ){
+                changeWord();
+                final Button speak = (Button)findViewById( R.id.b_speak );
+                speak.setVisibility( View.VISIBLE );
+            }
+        } ).setNegativeButton( "Ei", new DialogInterface.OnClickListener(){
+
+            public void onClick( DialogInterface dialog, int id ){
+                GuessingActivity.this.finish();
+            }
+        } );
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 
     private RecognitionListener rl = new RecognitionListener(){
@@ -48,13 +95,21 @@ public class GuessingActivity extends Activity{
 
         @Override
         public void onResults( Bundle results ){
+            final ProgressBar progress = (ProgressBar)findViewById( R.id.progress );
+            progress.setVisibility( View.GONE );
             List<String> result = results.getStringArrayList( SpeechRecognizer.RESULTS_RECOGNITION );
             if( result != null ){
                 for( String string : result ){
                     Log.d( TAG, "Result: " + string );
+                    boolean success = false;
+                    if( currentWord.equalsIgnoreCase( string ) ){
+                        success = true;
+                        Log.d( TAG, "We have a match!" );
+                    }
+                    showDialog( success, string );
                 }
             }
-            else {
+            else{
                 Log.d( TAG, "No results" );
             }
         }
@@ -73,14 +128,26 @@ public class GuessingActivity extends Activity{
 
         @Override
         public void onError( int error ){
-            Log.d( TAG, "Error: "+error );
+            final ProgressBar progress = (ProgressBar)findViewById( R.id.progress );
+            progress.setVisibility( View.GONE );
+            Log.d( TAG, "Error: " + error );
+            AlertDialog.Builder builder = new AlertDialog.Builder( GuessingActivity.this );
+            builder.setMessage( "Tehniline viga. Palun proovi uuesti." ).setCancelable( false ).setPositiveButton( "OK", new DialogInterface.OnClickListener(){
 
+                public void onClick( DialogInterface dialog, int id ){
+                    final Button speak = (Button)findViewById( R.id.b_speak );
+                    speak.setVisibility( View.VISIBLE );
+                }
+            } );
+            AlertDialog alert = builder.create();
+            alert.show();
         }
 
         @Override
         public void onEndOfSpeech(){
             Log.d( TAG, "User stopped talking" );
-
+            final ProgressBar progress = (ProgressBar)findViewById( R.id.progress );
+            progress.setVisibility( View.VISIBLE );
         }
 
         @Override
--- src/me/vermon/joomismang/JoomismangActivity.java
+++ src/me/vermon/joomismang/JoomismangActivity.java
@@ -1,58 +1,33 @@
 package me.vermon.joomismang;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 public class JoomismangActivity extends Activity{
 
-    private List<String> players = new ArrayList<String>();
 
     @Override
     public void onCreate( Bundle savedInstanceState ){
         super.onCreate( savedInstanceState );
         setContentView( R.layout.main );
-        final Button button = (Button)findViewById( R.id.b_one_player );
+        final Button button = (Button)findViewById( R.id.b_first );
         button.setOnClickListener( new View.OnClickListener(){
 
             public void onClick( View v ){
-                getPlayerNames( 1 );
+                Intent guessingIntent = new Intent(JoomismangActivity.this, GuessingActivity.class);
+                guessingIntent.putExtra( GuessingActivity.EXTRA_LEVEL, R.array.first_level );
+                startActivity( guessingIntent );
             }
         } );
-        final Button button2 = (Button)findViewById( R.id.b_two_players );
+        final Button button2 = (Button)findViewById( R.id.b_second );
         button2.setOnClickListener( new View.OnClickListener(){
 
             public void onClick( View v ){
-                getPlayerNames( 2 );
-            }
-        } );
-    }
-    
-//    @Override
-//    protected void onResume() {
-//        super.onResume();
-//        setContentView( R.layout.main );
-//    }
-
-    private void getPlayerNames( int playerCount ){
-        setContentView( R.layout.name );
-        TextView playerNameText = (TextView)findViewById( R.id.textView1 );
-        playerNameText.setText( "Sisesta esimese mängija nimi:" );
-        final EditText playerName = (EditText)findViewById( R.id.player );
-        playerName.setText( "Mängija1" );
-        final Intent guessingIntent = new Intent( this, GuessingActivity.class );
-        final Button insertName = (Button)findViewById( R.id.b_insert_name );
-        insertName.setOnClickListener( new View.OnClickListener(){
-
-            public void onClick( View v ){
-                players.add( playerName.getText().toString() );
+                Intent guessingIntent = new Intent(JoomismangActivity.this, GuessingActivity.class);
+                guessingIntent.putExtra( GuessingActivity.EXTRA_LEVEL, R.array.second_level );
                 startActivity( guessingIntent );
             }
         } );
